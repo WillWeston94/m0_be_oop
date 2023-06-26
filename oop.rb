56d8b9c1a41d580a8b92c2592795cc766e53180e
Will Weston <Williamweston81@gmail.com>
2023-06-26 12:22:11 -0500

    Weston(1) Create Hobbit class foundation before is_adult

--- oop.rb
+++ oop.rb
@@ -78,3 +78,17 @@ p dragon1
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is Frodo, true, if not, false.
+
+class Hobbit
+    attr_reader :name, :disposition, :age
+
+    def initialize(name, disposition, age = 0)
+        @name = name
+        @disposition = disposition
+        @age = age
+    end
+
+    def celebrate_birthday
+        @celebrate_birthday += 1 
+    end
+    
